0ef4ad54202f34f52f988d476f87c2f9d3a40b8c
Henry Harion <hariondeveloper@gmail.com>
2024-11-27 00:37:55 +0700

    Test API

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 // Module Dependencies
 // -------------------
 const { ServerConfig } = require("./bin/config");
-const morgan        = require("morgan");
-const express       = require('express');
-const bodyParser    = require('body-parser');
-const errorhandler  = require('errorhandler');
-const http          = require('http');
-const path          = require('path');
-const request       = require('request');
-const routes        = require('./bin/routes');
-const activity      = require('./bin/routes/activity');
+const morgan = require("morgan");
+const express = require('express');
+const bodyParser = require('body-parser');
+const errorhandler = require('errorhandler');
+const http = require('http');
+const path = require('path');
+const request = require('request');
+const routes = require('./bin/routes');
+const activity = require('./bin/routes/activity');
 
 const app = express();
 
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('port', ServerConfig.PORT);
-app.use(bodyParser.raw({type: 'application/jwt'}));
+app.use(bodyParser.raw({ type: 'application/jwt' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(express.methodOverride());
@@ -30,21 +30,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Express in Development Mode
 if ('development' == app.get('env')) {
-  app.use(errorhandler());
+    app.use(errorhandler());
 }
 
 // Hub Exchange Routes
-app.get('/', routes.index );
-app.post('/login', routes.login );
-app.post('/logout', routes.logout );
+app.get('/', routes.index);
+app.post('/login', routes.login);
+app.post('/logout', routes.logout);
 
 // Custom Activity Routes
-app.post('/journeybuilder/save/', activity.save );
-app.post('/journeybuilder/stop/', activity.stop );
-app.post('/journeybuilder/validate/', activity.validate );
-app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+app.post('/api/save/', activity.save);
+app.post('/api/stop/', activity.stop);
+app.post('/api/validate/', activity.validate);
+app.post('/api/publish/', activity.publish);
+app.post('/api/execute/', activity.execute);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('SFMC - JB - Activity App is listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('SFMC - JB - Activity App is listening on port ' + app.get('port'));
 });
