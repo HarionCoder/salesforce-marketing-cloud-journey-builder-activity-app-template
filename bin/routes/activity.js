e0d49f03faaf84065969b5c02b35bc02848c9553
HarionCoder <hariondeveloper@gmail.com>
2024-11-26 16:46:14 +0700

    try some thing

--- bin/routes/activity.js
+++ bin/routes/activity.js
@@ -92,7 +92,7 @@ exports.execute = async (req, res) => {
     const endpoint = 'https://api.telegram.org/bot';
     const url = `${endpoint}${token}/`;
 
-    const fln = req.body.ContactKey
+    let contactKey = req.body.keyValue;
 
     console.log(req.body);
     console.log(fln + ' Complete Request!')
